f29652647d50bd8433d1b3a1a9109aa1d8803b83
brunomaria71 <brunomaria71@yahoo.com>
2022-05-27 18:39:42 -0400

    added listen

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from "express";
-import cors fom 'cors';
+import cors from 'cors';
+import functions from 'firebase-functions';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
-
-app.listen(5050, () => {
-    console.log('Listening...');
-});
+export const api = functions.https.onRequest(app);
