2f86155e172f5a0eda490093240bdd22b662dde9
brunomaria71 <brunomaria71@yahoo.com>
2022-05-27 20:27:21 -0400

    trying to figure out this api

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import cors from 'cors';
 import functions from 'firebase-functions';
+import { addLoveMessage, deleteLoveMessage, getAllLoveMessages } from "./src/routes/loveMessagesRoutes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.post('/loveMessage', addLoveMessage);
+app.get('/loveMessages', getAllLoveMessages);
+app.get("/deleteLoveMessage", deleteLoveMessage);
+
+app.get('/test', (req, res) => {
+    res.send('This is actually working!');
+  });
 
 
 export const api = functions.https.onRequest(app);
---
+++ src/routes/loveMessagesRoutes.js
@@ -0,0 +1,47 @@
+import { loveMessagesCol } from "../gateway/connectDb.js";
+
+export const addLoveMessage = async (req, res) => {
+  if (!req.body.name || !req.body.message) {
+    response.status(401).send("Invalid request");
+    return;
+  }
+  const newloveMessage = {
+    name: req.body.name,
+    message: req.body.message,
+  };
+  try {
+    const doc = await loveMessagesCol.add(loveMessage);
+    res.status(201).send("loveMessage created " + doc.id);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export const getAllLoveMessages = async (req, res) => {
+  try {
+    const snapshot = await loveMessagesCol("loveMessages").get();
+    const loveMessagesArray = snapshot.docs.map((doc) => {
+      let loveMessage = doc.data();
+      loveMessage.id = doc.id;
+      return restaurant;
+    });
+    res.send(loveMessagesArray);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+ export function deleteLoveMessage(req, res) {
+  const { loveMessageId } = req.params;
+  if(!loveMessageId) {
+    res.status(401).send('Invalid request');
+    return;
+  }
+  loveMessagesCol.doc(loveMessageId).delete()
+    .then(() => {
+      res.send('Love Message deleted.');
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+}
