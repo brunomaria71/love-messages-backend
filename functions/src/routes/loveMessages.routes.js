0ed05fab86c8f8bc922fe10ac1c26d1f002340db
brunomaria71 <brunomaria71@yahoo.com>
2022-05-28 10:43:46 -0400

    changed key

--- functions/src/routes/loveMessages.routes.js
+++
@@ -1,9 +0,0 @@
-import { getAllLoveMessages } from '../services/loveMessages.services';
-
-app.get('/test', (req,res) => {
-    res.send('Wow I cannot believe this works!😱');
-});
-
-app.get('/loveMessages', getAllLoveMessages) 
-
-
